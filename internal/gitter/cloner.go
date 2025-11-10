88b5351152f568d6a4da2e3dc1445feacb314434
waste3d <kolya@waste3d.su>
2025-11-10 18:18:03 +0300

    feat: clonner

---
+++ internal/gitter/cloner.go
@@ -0,0 +1,28 @@
+package gitter
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-git/go-git/v5"
+)
+
+func CloneToTemp(url string) (string, error) {
+	tempDir, err := os.MkdirTemp("", "auto-ci-clone-*")
+	if err != nil {
+		return "", fmt.Errorf("не удалось создать временную директорию: %w", err)
+	}
+
+	fmt.Printf("Клонирование репозитория %s...\n", url)
+
+	_, err = git.PlainClone(tempDir, false, &git.CloneOptions{
+		URL: url,
+	})
+	if err != nil {
+		os.RemoveAll(tempDir)
+		return "", fmt.Errorf("не удалось клонировать репозиторий: %w", err)
+	}
+
+	fmt.Println("Репозиторий успешно склонирован.")
+	return tempDir, nil
+}
